f3e2a1597a31a926522a1963523fba8b8925ca01
Gonzalo <mgmediaweb@gmail.com>
2022-06-20 18:58:14 -0700

    update buttons dynamic links

--- src/index.js
+++ src/index.js
@@ -13,13 +13,17 @@ const modalComments = document.getElementById('modalComments');
 
 btnComments.forEach(btn => {
     btn.addEventListener('click', () => {
+        const id = modal.getAttribute('data-bs-id');
         loadModalInfo(btn);
     })
 });
 
 btnReservations.forEach(btn => {
     btn.addEventListener('click', () => {
+        const idnum = btn.getAttribute('data-bs-id');
         loadModalInfo(btn);
+
+        Reservations.get(idnum);
     })    
 });
 
@@ -27,26 +31,26 @@ btnReservations.forEach(btn => {
 formReservation.addEventListener('submit', (event) => {
     event.preventDefault();
 
+    const id = document.getElementById('modalReservId');
     const name = document.getElementById('reservationName');
     const start = document.getElementById('reservationStart');
     const end = document.getElementById('reservationEnd');
 
-    console.log("form");
+    Reservations.add(id.value, name.value, start.value, end.value);
 
-    Reservations.add(Config.InvolveID, name, start, end);
-
+    id.value = '';
     name.value = '';
     start.value = '';
     end.value = '';
 });
 
 const loadModalInfo = modal => {
-    var name = modal.getAttribute('data-bs-name');
-    var country = modal.getAttribute('data-bs-country');
-    var team = modal.getAttribute('data-bs-team');
-    var id = modal.getAttribute('data-bs-id');
-    var link = modal.getAttribute('data-bs-link');
-    var image = modal.getAttribute('data-bs-image');
+    const id = modal.getAttribute('data-bs-id');
+    const name = modal.getAttribute('data-bs-name');
+    const country = modal.getAttribute('data-bs-country');
+    const team = modal.getAttribute('data-bs-team');
+    const link = modal.getAttribute('data-bs-link');
+    const image = modal.getAttribute('data-bs-image');
     
     document.getElementById("modalCommentName").innerHTML = name; 
     document.getElementById("modalCommentCountry").innerHTML = country; 
@@ -54,6 +58,7 @@ const loadModalInfo = modal => {
     document.getElementById("modalCommentPlayer").innerHTML = id; 
     document.getElementById("modalCommentLink").href = link; 
     document.getElementById("modalCommentImage").src = image;
+    document.getElementById("modalCommentId").value = id;
 
     document.getElementById("modalReservName").innerHTML = name;
     document.getElementById("modalReservCountry").innerHTML = country; 
@@ -61,7 +66,7 @@ const loadModalInfo = modal => {
     document.getElementById("modalReservPlayer").innerHTML = id;
     document.getElementById("modalReservLink").href = link; 
     document.getElementById("modalReservImage").src = image;
+    document.getElementById("modalReservId").value = id;
 }
 
 //createGame(Storage);
-//Reservations.get(Config.InvolveID);
--- src/modules/reservations.js
+++ src/modules/reservations.js
@@ -1,36 +1,60 @@
 import Config from './config.js';
 
-const getData = async (id) => {
-    //console.log(`${Config.InvolveAPI}apps/${id}/reservations`);
-    const response = await fetch(`${Config.InvolveAPI}apps/${id}/reservations?item_id=55`, {
+const boardReservation = document.getElementById('msjReservations');
+
+const showData = data => {
+    let content = '';
+    boardReservation.innerHTML = '';
+
+    let reservCount = data.length;
+    if(reservCount == undefined) reservCount = 0;
+    
+    document.getElementById('title-reservation').innerHTML = `Reservations (${reservCount})`;
+  
+    if (data.length) {
+        data.map((item) => {
+            content += `<p>${item.date_start} - ${item.date_end} <span class="fw-bold">By ${item.username}</span></p>`;
+            return true;
+        });
+    } else {
+      content += '<p class="text-center py-3">No reservation availables</p>';
+    }
+  
+    boardReservation.innerHTML = content;
+};
+
+const getData = async (item) => {
+    //console.log(`${Config.InvolveAPI}apps/${Config.InvolveID}/reservations?item_id=${item}`);
+    const response = await fetch(`${Config.InvolveAPI}apps/${Config.InvolveID}/reservations?item_id=${item}`, {
         method: 'GET',
         headers: {
         'Content-type': 'application/json; charset=UTF-8',
         },
     });
     const results = await response.json();
-
     //console.log(results);
-
-    //showScores(results.result);
+    showData(results);
 };
 
-const addData = async (id, name, start, end) => {
-    //console.log("@@@ ", id, name, start, end);
-    /*
-    const response = await fetch(`${Config.InvolveAPI}games/${id}/scores`, {
+const addData = async (item, name, start, end) => {
+
+    const sendInfo = {
+        item_id: item,
+        username: name,
+        date_start: start,
+        date_end: end
+    }
+
+    const response = await fetch(`${Config.InvolveAPI}apps/${Config.InvolveID}/reservations`, {
         method: 'POST',
-        body: JSON.stringify({
-            user,
-            score: Number(score),
-        }),
+        body: JSON.stringify(sendInfo),
         headers: {
         'Content-type': 'application/json; charset=UTF-8',
         },
     });
-    await response.json();
-    //getData(id);
-    */
+    const results = await response.json();
+    console.log("@@@ ", results);
+    getData(item);
 };
 
 export default class Reservations {
