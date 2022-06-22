bbb9df3b17df52f2a8d9c22b2cf5e39b7f054ec5
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-06-22 12:34:28 -0500

    Fix linters and re compile

--- src/modules/config.js
+++ src/modules/config.js
@@ -1,4 +1,4 @@
 export default {
   InvolveAPI: 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/',
-  InvolveID: 'Xkh1FfY1mMypcFx6PTxq',  
+  InvolveID: 'Xkh1FfY1mMypcFx6PTxq',
 };
