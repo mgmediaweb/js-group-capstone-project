548f8acfa945abf89373caf81da526cb59d7912d
Gonzalo <mgmediaweb@gmail.com>
2022-06-22 10:16:49 -0700

    clear the api

--- src/modules/config.js
+++ src/modules/config.js
@@ -1,4 +1,5 @@
 export default {
   InvolveAPI: 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/',
-  InvolveID: 'eNwEldadBkqhnwvLXJvh',
+  //InvolveID: 'eNwEldadBkqhnwvLXJvh',
+  InvolveID: 'Xkh1FfY1mMypcFx6PTxq',  
 };
