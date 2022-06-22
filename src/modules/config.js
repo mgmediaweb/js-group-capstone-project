01432513a46e15ffe94d4f7b9087ee680d203888
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-06-22 12:21:05 -0500

    Compile the app again and remove comments

--- src/modules/config.js
+++ src/modules/config.js
@@ -1,5 +1,4 @@
 export default {
   InvolveAPI: 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/',
-  //InvolveID: 'eNwEldadBkqhnwvLXJvh',
   InvolveID: 'Xkh1FfY1mMypcFx6PTxq',  
 };
